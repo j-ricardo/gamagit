2f0ffd6efdefca5fc41d1135ba135ffeccaca25b
Ricardo <ricardo.junior_@outlook.com>
2021-07-14 21:54:12 -0300

    adição de rotas

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import React, { useState } from "react";
-import axios from 'axios';
-function App(props) {  
-  const [usuario, setUsuario ] = useState('j-ricardo');
-  function handleSearch(){     
-    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response.data))
-  }
+import React from "react";
+import Routes from './routes';
+
+function App() {  
+  
   return (    
-    <>
-    <h4>Busca Repositórios GIT</h4>
-      <input name="usuario" id="usuario" className="usuarioInput" placeholder="Usuário" value={usuario} onChange={e => setUsuario(e.target.value)}/> 
-      <button type="button" onClick={handleSearch}>Pesquisar</button><br/>
-      <p>Observe o console</p>
-    </>    
+    <Routes />
   );
 }
 
